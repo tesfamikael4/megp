1faef5a795f0654ed6df53be1466bb40c2e0ec27
Misikir Adane <misikir.adane@gmail.com>
2023-10-10 03:41:51 +0300

    ci: test

---
+++ apps/back-office/iam-fe/.bin/docker-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change directory to the root directory of your project
+cd ../../../
+
+# Build the Docker image using the specified Dockerfile
+docker build -f apps/back-office/iam-fe/Dockerfile -t iam-fe .
+
+# Tag the Docker image with a custom repository and tag
+docker tag iam-fe:latest gitlab.peragosystems.com:5050/megp/megp/iam-fe:latest
+
+# Push the tagged Docker image to the remote repository
+docker push gitlab.peragosystems.com:5050/megp/megp/iam-fe:latest
+
+# Optionally, remove the local Docker image if you don't need it anymore
+# docker rmi iam-fe:latest
