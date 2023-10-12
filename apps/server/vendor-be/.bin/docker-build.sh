c18137b6b62cabdb44e1012edcd4b5cad7edd14c
Administrator <admin@example.com>
2023-10-12 10:57:17 +0000

    Update 2 files
    - /apps/server/vendor-be/.bin/docker-build.sh
    - /apps/server/vendor-be/package.json

--- apps/server/vendor-be/.bin/docker-build.sh
+++ apps/server/vendor-be/.bin/docker-build.sh
@@ -7,7 +7,7 @@ cd ../../../
 docker build -f apps/server/vendor-be/Dockerfile -t vendor-be .
 
 # Tag the Docker image with a custom repository and tag
-docker tag iam-fe:latest gitlab.peragosystems.com:5050/megp/megp/vendor-be:latest
+docker tag vendor-be:latest gitlab.peragosystems.com:5050/megp/megp/vendor-be:latest
 
 # Push the tagged Docker image to the remote repository
 docker push gitlab.peragosystems.com:5050/megp/megp/vendor-be:latest
