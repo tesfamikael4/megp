36c576edd50b3558d9848dac09e64b39d8e3a6b3
Administrator <admin@example.com>
2023-10-17 18:21:27 +0000

    Update main.ts

--- apps/server/vendor-be/src/main.ts
+++ apps/server/vendor-be/src/main.ts
@@ -9,6 +9,7 @@ import {
   SwaggerModule,
 } from '@nestjs/swagger';
 
+import { v4 as uuidv4 } from 'uuid';
 import { Server } from '@tus/server';
 import { S3Store } from '@tus/s3-store';
 import * as Minio from 'minio';
