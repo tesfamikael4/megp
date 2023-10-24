a6992c49d1767378c50548ba422439eb51f4e28a
Administrator <admin@example.com>
2023-10-24 07:37:45 +0000

    Add new file

---
+++ apps/server/planning-be/.bin/docker-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Change directory to the root directory of your project
+cd ../../../
+
+# Build the Docker image using the specified Dockerfile
+docker build -f apps/server/planning-be/Dockerfile -t planning-be .
+
+
+# Tag the Docker image with a custom repository and tag
+docker tag planning-be:latest gitlab.peragosystems.com:5050/megp/megp/planning-be:latest
+
+# Push the tagged Docker image to the remote repository
+docker push gitlab.peragosystems.com:5050/megp/megp/planning-be:latest
+
+# Optionally, remove the local Docker image if you don't need it anymore
+# docker rmi iam-fe:latest
