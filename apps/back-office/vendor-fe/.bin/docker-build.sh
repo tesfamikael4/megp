c8d88048cbf843ed47745124b2287c2000f6785d
Administrator <admin@example.com>
2023-11-01 14:02:18 +0000

    Update docker-build.sh

--- apps/back-office/vendor-fe/.bin/docker-build.sh
+++ apps/back-office/vendor-fe/.bin/docker-build.sh
@@ -7,10 +7,10 @@ cd ../../../
 docker build -f apps/back-office/vendor-fe/Dockerfile -t vendor-fe .
 
 # Tag the Docker image with a custom repository and tag
-docker tag vendor-fe:latest gitlab.peragosystems.com:5050/megp/megp/vendor-fe:v1
+docker tag vendor-fe:latest gitlab.peragosystems.com:5050/megp/megp/vendor-fe:latest
 
 # Push the tagged Docker image to the remote repository
-docker push gitlab.peragosystems.com:5050/megp/megp/vendor-fe:v1
+docker push gitlab.peragosystems.com:5050/megp/megp/vendor-fe:latest
 
 # Optionally, remove the local Docker image if you don't need it anymore
 # docker rmi iam-fe:latest
