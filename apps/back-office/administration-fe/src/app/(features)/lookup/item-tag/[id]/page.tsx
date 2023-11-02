a87822c38f6a0d2a8c231549b1811064806052ee
fmoges@excellerentsolutions.com <fmoges@excellerentsolutions.com>
2023-11-02 17:18:26 +0300

    feat: item tag ui created

---
+++ apps/back-office/administration-fe/src/app/(features)/lookup/item-tag/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { ItemTagForm } from '../_components/form-detail';
+
+export default function ItemTagDetailForm() {
+  return <ItemTagForm mode="detail" />;
+}
--- apps/back-office/administration-fe/src/store/entity/list.ts
+++ apps/back-office/administration-fe/src/store/entity/list.ts
@@ -11,4 +11,9 @@ export const ENTITY_LIST: EntityStoreConfig[] = [
     baseUrl:
       process.env.NEXT_PUBLIC_ADMINISTRATION_API ?? '/api/administration',
   },
+  {
+    entity: 'tag',
+    baseUrl:
+      process.env.NEXT_PUBLIC_ADMINISTRATION_API ?? '/api/administration',
+  },
 ];
