7481c4ebb7303f3d218a7fb91e49c8c7b76751a3
Misikir Adane <misikir.adane@gmail.com>
2023-11-06 01:26:05 +0300

    ci: -y

--- apps/portal/egp/.bin/gitops.sh
+++ apps/portal/egp/.bin/gitops.sh
@@ -28,7 +28,8 @@ cd "gitops/applications"
 
 # https://mikefarah.gitbook.io/yq/v/v3.x/
  
-yq e -i "$APP_NAME.tag = $CI_COMMIT_SHORT_SHA" values-dev.yaml
+
+yq -y eval "${$APP_NAME}.tag = \"$CI_COMMIT_SHORT_SHA\"" -i values-dev.yaml
 
 # Display the contents of values-dev.yaml
 cat values-dev.yaml
