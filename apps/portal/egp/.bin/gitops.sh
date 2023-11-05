5cee48687695a4c48521a773905810ba3e50725d
Misikir Adane <misikir.adane@gmail.com>
2023-11-06 02:15:25 +0300

    ci: verbose

--- apps/portal/egp/.bin/gitops.sh
+++ apps/portal/egp/.bin/gitops.sh
@@ -29,7 +29,7 @@ cd "gitops/applications"
 # https://mikefarah.gitbook.io/yq/v/v3.x/
 
 
-yq -i values-dev.yaml $APP_NAME.tag  $CI_COMMIT_SHORT_SHA -y
+yq -i  "$APP_NAME.tag = $CI_COMMIT_SHORT_SHA" values-dev.yaml -y -v
 
  
 # Display the contents of values-dev.yaml
