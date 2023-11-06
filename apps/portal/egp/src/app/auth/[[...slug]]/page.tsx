34ea8b978788bd65a34382e730bfeb94988c9318
gitops <gitops@peragosystems.com>
2023-11-06 03:23:15 +0300

    feat: css

--- apps/portal/egp/src/app/auth/[[...slug]]/page.tsx
+++ apps/portal/egp/src/app/auth/[[...slug]]/page.tsx
@@ -1,5 +1,6 @@
 import { Auth } from '@megp/auth';
+import { theme as baseTheme } from '@/utilities/theme';
 export default function AuthPage({ params }: { params: { slug: string } }) {
   // import auth
-  return <Auth path={params.slug} />;
+  return <Auth path={params.slug} theme={baseTheme} />;
 }
--- packages/fe/auth/src/components/layout/auth.tsx
+++ packages/fe/auth/src/components/layout/auth.tsx
@@ -1,16 +1,17 @@
 'use client';
-import { Flex, Title, Divider, Box } from '@mantine/core';
+import { Box, Divider, Flex, MantineProvider, Title } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { theme as baseTheme } from '@megp/theme/mantine';
 import Image from 'next/image';
+import ForgotPassword from '../auth/forgot-password/forgot-password';
 import Signin from '../auth/login/login';
-import SignUp from '../auth/signup/signup';
 import SecurityQPassReset from '../auth/questions-reset/questions-reset';
 import PasswordReset from '../auth/reset-password/reset-password';
-import ForgotPassword from '../auth/forgot-password/forgot-password';
 import SetSecurity from '../auth/setSecurity/set-security';
+import SignUp from '../auth/signup/signup';
 import Otp from '../auth/verification/otp';
-import workflow from './workflow.png';
 import styles from './auth.module.scss';
-import '@mantine/core/styles.css';
+import workflow from './workflow.png';
 
 const page = {
   login: <Signin />,
@@ -21,57 +22,66 @@ const page = {
   'set-security': <SetSecurity />,
 };
 
-export function Auth({ path }: { path: string }): JSX.Element {
+export function Auth({
+  path,
+  theme = baseTheme,
+}: {
+  path: string;
+  theme?: any;
+}): JSX.Element {
   const render = path[0].startsWith('verification') ? (
     <Otp id={path[1]} />
   ) : (
     page[path]
   );
   return (
-    <div className="flex flex-row">
-      <div className={styles.left_content}>
-        <Image
-          alt="Malawi Republic"
-          className={styles.logo}
-          height={100}
-          src="/ppda.png"
-          width={100}
-        />
-        <Box>{render}</Box>
-        <div className="absolute bottom-0">
-          <p className={styles.footer_text}>
-            Copyright &copy; 2023, Procurement and Disposal of Assets Authority.
-          </p>
-          <Flex className={styles.footer_text}>
-            <p className="mt-2">Powered by </p>
-            <Image
-              alt="logo"
-              className="mt-2 ml-1"
-              height={30}
-              src="/perago.png"
-              width={80}
-            />
-          </Flex>
+    <MantineProvider theme={theme}>
+      <div className="flex flex-row">
+        <div className={styles.left_content}>
+          <Image
+            alt="Malawi Republic"
+            className={styles.logo}
+            height={100}
+            src="/ppda.png"
+            width={100}
+          />
+          <Box>{render}</Box>
+          <div className="absolute bottom-0">
+            <p className={styles.footer_text}>
+              Copyright &copy; 2023, Procurement and Disposal of Assets
+              Authority.
+            </p>
+            <Flex className={styles.footer_text}>
+              <p className="mt-2">Powered by </p>
+              <Image
+                alt="logo"
+                className="mt-2 ml-1"
+                height={30}
+                src="/perago.png"
+                width={80}
+              />
+            </Flex>
+          </div>
+        </div>
+        <div className={styles.right_content}>
+          <div
+            className={styles.workflow}
+            style={{ backgroundImage: `url(${workflow.src})` }}
+          />
+          <Title className={styles.welcome}>Welcome to eGP Malawi</Title>
+          <Divider my="sm" />
+          <div className="w-2/3 bg-white rounded-sm">
+            <p className="text-center justify-center text-lg">
+              The eGP Malawi Platform is a web-based, collaborative system to
+              manage the full lifecycle of a tendering and contract management
+              process, for both government agencies and suppliers. It offers a
+              secure, interactive, dynamic environment for procurements of any
+              nature, complexity or value, enforcing compliance to regulations,
+              and encouraging recognized best practices.
+            </p>
+          </div>
         </div>
       </div>
-      <div className={styles.right_content}>
-        <div
-          className={styles.workflow}
-          style={{ backgroundImage: `url(${workflow.src})` }}
-        />
-        <Title className={styles.welcome}>Welcome to eGP Malawi</Title>
-        <Divider my="sm" />
-        <div className="w-2/3 bg-white rounded-sm">
-          <p className="text-center justify-center text-lg">
-            The eGP Malawi Platform is a web-based, collaborative system to
-            manage the full lifecycle of a tendering and contract management
-            process, for both government agencies and suppliers. It offers a
-            secure, interactive, dynamic environment for procurements of any
-            nature, complexity or value, enforcing compliance to regulations,
-            and encouraging recognized best practices.
-          </p>
-        </div>
-      </div>
-    </div>
+    </MantineProvider>
   );
 }
