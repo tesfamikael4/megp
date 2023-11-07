cd34410cb2ed53a3f8ec2a9fa4a0fc4003dce93a
demeke <demeke.get23@gmail.com>
2023-11-07 10:01:24 +0300

    priceing by key

--- apps/server/vendor-be/src/pricing/service-pricing.service.ts
+++ apps/server/vendor-be/src/pricing/service-pricing.service.ts
@@ -1,8 +1,9 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { ServicePrice } from './entities/service-price';
 import { EntityCrudService } from 'src/shared/service';
+import { ServiceKeyEnum } from 'src/handling/dtos/workflow-instance.enum';
 @Injectable()
 export class ServicePricingService extends EntityCrudService<ServicePrice> {
   constructor(
@@ -10,5 +11,35 @@ export class ServicePricingService extends EntityCrudService<ServicePrice> {
     private readonly pricingRepository: Repository<ServicePrice>,
   ) {
     super(pricingRepository);
+  }
+
+  async findServicePriceByServiceType(serviceKey: string) {
+    let keys = [];
+    if (serviceKey === ServiceKeyEnum.new) {
+      keys = [
+        ServiceKeyEnum.goodsNewRegistration,
+        ServiceKeyEnum.servicesNewRegistration,
+        ServiceKeyEnum.worksNewRegistration,
+      ];
+    } else if (serviceKey == ServiceKeyEnum.upgrade) {
+      keys = [
+        ServiceKeyEnum.goodsUpgrade,
+        ServiceKeyEnum.servicesUpgrade,
+        ServiceKeyEnum.worksUpgrade,
+      ];
+    } else if (serviceKey === ServiceKeyEnum.renewal) {
+      keys = [
+        ServiceKeyEnum.goodsRenewal,
+        ServiceKeyEnum.servicesRenewal,
+        ServiceKeyEnum.worksRenewal,
+      ];
+    }
+    return await this.pricingRepository.find(
+      {
+        select: { id: true, serviceId: true, businessArea: true, valueFrom: true, valueTo: true, fee: true, currency: true, service: { businessProcesses: { id: true }, id: true, key: true } },
+        relations: { service: { businessProcesses: true } },
+        where: { service: { businessProcesses: { isActive: true, }, key: In(keys) } }
+
+      })
   }
 }
