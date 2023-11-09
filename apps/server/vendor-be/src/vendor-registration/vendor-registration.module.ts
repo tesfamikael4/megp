0e69a4890dfd8c7fdbd63489243eb7b3d52ccb55
demeke <demeke.get23@gmail.com>
2023-11-09 08:39:40 +0300

    workflow refactoring

--- apps/server/vendor-be/src/vendor-registration/vendor-registration.module.ts
+++ apps/server/vendor-be/src/vendor-registration/vendor-registration.module.ts
@@ -30,17 +30,13 @@ import { PaymentReceiptEntity } from 'src/handling/entities/receipt-attachment';
 import { WorkflowInstanceController } from 'src/handling/controllers/workflow-instance.controller';
 import { ApplicationExcutionController } from 'src/handling/controllers/application-execution.controller';
 import { ApplicationExcutionService } from 'src/handling/services/application-execution.service';
-import { TaskEntity } from 'src/bpm/entities/task.entity';
 import { ServicePrice } from 'src/pricing/entities/service-price';
 import { ServicePricingService } from 'src/pricing/service-pricing.service';
 import { ServicePricingController } from 'src/pricing/service-pricing.controller';
 import { HandlingCommonService } from 'src/handling/services/handling-common-services';
 import { TusService } from './services/tus.service';
 import { UploadController } from './controllers/file.controller';
-import { TaskService } from 'src/bpm/services/task.service';
-import { BusinessProcessService } from 'src/bpm/services/business-process.service';
 import { BankAccountDetailEntity } from './entities/bank-account-detail.entity';
-import { WorkflowService } from 'src/bpm/services/workflow.service';
 import { BpmModule } from 'src/bpm/bpm.module';
 import { TaskAssignmentEntity } from 'src/bpm/entities/task-assignment';
 import { TaskHandlerEntity } from 'src/bpm/entities/task-handler';
@@ -59,16 +55,14 @@ import { TaskTrackerEntity } from 'src/bpm/entities/task-tracker';
       BeneficialOwnership,
       AreasOfBusinessInterestEntity,
       PaymentReceiptEntity,
-      //temporary, will be removed when modularization complete
-      TaskEntity,
+      BpServiceEntity,
+      //temporary, will be removed when modularization complete      
       WorkflowInstanceEntity,
       InvoiceEntity,
-      TaskEntity,
       TaskAssignmentEntity,
       TaskHandlerEntity,
       TaskTrackerEntity,
-      BusinessProcessEntity,
-      BpServiceEntity,
+
     ]),
     BpmModule,
   ],
@@ -76,7 +70,6 @@ import { TaskTrackerEntity } from 'src/bpm/entities/task-tracker';
   providers: [
     ServicePricingService,
     VendorBasicsService,
-    BusinessProcessService,
     HandlingCommonService,
     BankAccountDetailService,
     ShareholderService,
