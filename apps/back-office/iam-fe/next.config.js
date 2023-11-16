6195908d7d6ce6eb2051bdf8af0261c07067134e
gitops <gitops@peragosystems.com>
2023-11-16 14:23:07 +0300

    ci: iam trigger

--- apps/back-office/iam-fe/next.config.js
+++ apps/back-office/iam-fe/next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
     {
       test: /\.svg$/i,
       issuer: fileLoaderRule.issuer,
-      resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+      resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, 
       use: ['@svgr/webpack'],
     }
   );
