1fc250fb2f024d05ac6b66289899cfacecb93b74
yayasoles <yayasoles@gmail.com>
2023-11-17 23:45:19 +0300

    feat(vendor-be): vendor-status change done, vendor Isr

--- apps/server/vendor-be/src/entities/business-area.ts
+++ apps/server/vendor-be/src/entities/business-area.ts
@@ -8,7 +8,6 @@ import {
 } from 'typeorm';
 import { VendorsEntity } from './vendors.entity';
 import { ParseIntPipe } from '@nestjs/common';
-import { Category } from './category.entity';
 
 @Entity({ name: 'business-areas' })
 export class BusinessAreaEntity {
@@ -17,16 +16,16 @@ export class BusinessAreaEntity {
   @Column({ type: 'uuid' })
   vendorId: string;
   @Column({ type: 'uuid' })
-  categoryId: string;
+  instanceId: string;
+  @Column({ type: 'uuid' })
+  category: string;
   @Column({ default: new Date() })
   approvedAt: Date;
   @Column({ default: () => `CURRENT_TIMESTAMP + INTERVAL '1 year'` })
   expireDate: Date;
   @Column({ default: 'Active' })
   status: string;
-  @ManyToOne(() => Category, (category) => category.businessCategories)
-  @JoinColumn({ name: 'categoryId' })
-  category: Category;
+
   @ManyToOne(() => VendorsEntity, (vendor) => vendor.businessCats)
   @JoinColumn({ name: 'vendorId' })
   vendor: VendorsEntity;
