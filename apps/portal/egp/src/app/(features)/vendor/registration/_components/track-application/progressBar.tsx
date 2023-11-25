1bfa20fa04af1a85c6d9061476a8498b5f528ada
Eyuel Belete <eyuelbelete98@gmail.com>
2023-11-25 09:22:52 +0000

    Vendor work space v4

--- apps/portal/egp/src/app/(features)/vendor/registration/_components/track-application/progressBar.tsx
+++ apps/portal/egp/src/app/(features)/vendor/registration/_components/track-application/progressBar.tsx
@@ -7,48 +7,59 @@ import {
   IconForms,
   IconCheck,
   IconNotification,
+  IconCertificate,
 } from '@tabler/icons-react';
 
 export default function ProgressBar() {
   return (
-    <Timeline active={1} bulletSize={24} lineWidth={2}>
-      <Timeline.Item bullet={<IconForms size={12} />} title="Submitted">
+    <Timeline active={2} bulletSize={24} lineWidth={2}>
+      <Timeline.Item
+        bullet={<IconForms size={12} />}
+        title="Submitted Vendor Registration Request"
+      >
         <Text c="dimmed" size="sm">
-          You&apos;ve created new branch{' '}
-        </Text>
-        <Text size="xs" mt={4}>
-          2 hours ago
-        </Text>
-      </Timeline.Item>
-
-      <Timeline.Item bullet={<IconCheck size={12} />} title="Review">
-        <Text c="dimmed" size="sm">
-          You&apos;ve pushed 23 commits to
+          Remark: You&apos;ve created new branch{' '}
         </Text>
       </Timeline.Item>
 
       <Timeline.Item
-        title="Pull request"
+        bullet={<IconCheck size={12} />}
+        title="Reviewed Vendor Registration Request"
+      >
+        <Text c="dimmed" size="sm">
+          Remark: You&apos;ve pushed 23 commits to
+        </Text>
+      </Timeline.Item>
+
+      <Timeline.Item
+        title="Approval of Vendor Registration Request By CRO"
         bullet={<IconNotification size={12} />}
         lineVariant="dashed"
       >
         <Text c="dimmed" size="sm">
-          Notification
-        </Text>
-        <Text size="xs" mt={4}>
-          34 minutes ago
+          Remark: Notification
         </Text>
       </Timeline.Item>
 
-      <Timeline.Item title="Code review" bullet={<IconMessageDots size={12} />}>
+      <Timeline.Item
+        title="Final Approval of New Vendor Registration by RRM"
+        bullet={<IconCheck size={12} />}
+      >
         <Text c="dimmed" size="sm">
           <Text variant="link" component="span" inherit>
-            Robert Gluesticker
-          </Text>{' '}
-          left a code review on your pull request
+            Remark: Robert Gluesticker
+          </Text>
         </Text>
-        <Text size="xs" mt={4}>
-          12 minutes ago
+      </Timeline.Item>
+
+      <Timeline.Item
+        title="Generate Vendor Registration Certificate"
+        bullet={<IconCertificate size={12} />}
+      >
+        <Text c="dimmed" size="sm">
+          <Text variant="link" component="span" inherit>
+            Remark: Robert Gluesticker
+          </Text>
         </Text>
       </Timeline.Item>
     </Timeline>
