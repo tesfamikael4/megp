019ff3a1a5436f4529074889dd68fb53e2d390da
Ermias Tefera <wteffera11@gmail.com>
2023-11-28 21:46:55 +0000

    Fix breadcrumb overflow

--- apps/back-office/administration-fe/src/store/api/taxonomies/taxonomies.api.ts
+++ apps/back-office/administration-fe/src/store/api/taxonomies/taxonomies.api.ts
@@ -4,7 +4,9 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 export const taxonomiesApi = createApi({
   reducerPath: 'taxonomiesApi',
   tagTypes: ['taxonomies'],
-  baseQuery: baseQuery(process.env.NEXT_PUBLIC_ADMINISTRATION_API ?? '/'),
+  baseQuery: baseQuery(
+    process.env.NEXT_PUBLIC_ADMINISTRATION_API ?? '/administration/api/',
+  ),
   endpoints: (build) => ({
     createTaxonomies: build.mutation({
       query: (body) => ({
