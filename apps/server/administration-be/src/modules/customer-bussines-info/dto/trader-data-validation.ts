24c8ac56f077760c7c4d6941a2d369cdfb2b5924
unknown <ydesta3614@gmail.com>
2023-11-29 16:07:49 +0300

    feat(administration-be): trader validation

--- apps/server/administration-be/src/modules/customer-bussines-info/dto/trader-data-validation.ts
+++ apps/server/administration-be/src/modules/customer-bussines-info/dto/trader-data-validation.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { CreateCustomerBussinesInfoDto } from './create-customer-bussines-info.dto';
 import { IsArray } from 'class-validator';
-import { CustomerBussinesInfo } from 'src/entities/customer-bussines-info.entity';
+import { CustomerBussinesInfo } from '@entities';
 
 export class TraderDataValidation {
   @ApiProperty({ isArray: true, type: () => CustomerBussinesInfo })
