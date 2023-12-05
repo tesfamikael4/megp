4d9fffb2baf495720ffdbd64e4e59cbc01537549
ermilla <wteffera11@gmail.com>
2023-12-05 15:05:24 +0300

    fix(administration-be): fix item category tree

--- apps/server/administration-be/src/modules/look-up-data/service/item-category.service.ts
+++ apps/server/administration-be/src/modules/look-up-data/service/item-category.service.ts
@@ -1,11 +1,10 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { ItemCategoryResponseDto } from '../dto/item-category.dto';
 import { DataResponseFormat } from 'src/shared/api-data';
 import { ItemCategory } from 'src/entities/item-category.entity';
 import { EntityCrudService } from 'src/shared/service';
-import { CollectionQuery } from 'src/shared/collection-query';
+import { CollectionQuery, QueryConstructor } from 'src/shared/collection-query';
 @Injectable()
 export class ItemCategoryService extends EntityCrudService<ItemCategory> {
   constructor(
@@ -14,39 +13,26 @@ export class ItemCategoryService extends EntityCrudService<ItemCategory> {
   ) {
     super(itemCatRepository);
   }
-  async findAllCateAndSubCat(
-    query: CollectionQuery,
-  ): Promise<DataResponseFormat<ItemCategoryResponseDto>> {
-    try {
-      const response = await super.findAll(query);
-      const newResponse: DataResponseFormat<ItemCategoryResponseDto> = {
-        total: response.total,
-        items: this.buildCatAndSubCat(response.items, null),
-      };
-      return newResponse;
-    } catch (error) {
-      throw new HttpException(error, HttpStatus.BAD_REQUEST);
-    }
+  async findItems(query: CollectionQuery) {
+    console.log(query);
+    return await this.getClassifications(query);
   }
 
-  private buildCatAndSubCat(
-    items: ItemCategory[],
-    parentId: string | null,
-  ): ItemCategoryResponseDto[] {
-    const catTree: ItemCategoryResponseDto[] = [];
+  private async getClassifications(query: CollectionQuery) {
+    const dataQuery = QueryConstructor.constructQuery<ItemCategory>(
+      this.itemCatRepository,
+      query,
+    );
 
-    items.forEach((item) => {
-      if (item.parentId === parentId) {
-        const newItem: ItemCategoryResponseDto = {
-          id: item.id,
-          name: item.name,
-          parentId: item.parentId,
-          childCategories: this.buildCatAndSubCat(items, item.id),
-        };
-        catTree.push(newItem);
-      }
-    });
+    const response = new DataResponseFormat<ItemCategory>();
+    if (query.count) {
+      response.total = await dataQuery.getCount();
+    } else {
+      const [result, total] = await dataQuery.getManyAndCount();
+      response.total = total;
+      response.items = result;
+    }
 
-    return catTree;
+    return response;
   }
 }
