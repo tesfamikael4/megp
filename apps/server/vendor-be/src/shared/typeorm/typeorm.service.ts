ca9fafbef52ff78fe9fc74050cf6ef1f97b983c9
zolaDO <zdoti55@gmail.com>
2023-12-15 09:26:05 +0300

    feat(vendor-be): update on docker

--- apps/server/vendor-be/src/shared/typeorm/typeorm.service.ts
+++ apps/server/vendor-be/src/shared/typeorm/typeorm.service.ts
@@ -3,7 +3,6 @@ import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { dataSourceOptions } from './typeorm-config-helper';
 import { SeederOptions } from 'typeorm-extension';
 
-
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   public createTypeOrmOptions(): TypeOrmModuleOptions & SeederOptions {
