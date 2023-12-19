c91005d4524b4d0ce22114685dae611f3019a755
zolaDO <zdoti55@gmail.com>
2023-12-19 11:03:50 +0300

    feat(iam-be): role filter added

--- apps/server/iam-be/src/modules/role/role.module.ts
+++ apps/server/iam-be/src/modules/role/role.module.ts
@@ -6,11 +6,13 @@ import { RoleService } from './services/role.service';
 import { RolePermissionNewController } from './controllers/role-permission.controller';
 import { RolePermissionService } from './services/role-permission.service';
 import { ApplicationModule } from '../application/application.module';
+import { AuthorizationModule } from 'src/shared/authorization';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Role, RolePermission]),
     ApplicationModule,
+    AuthorizationModule,
   ],
   providers: [RoleService, RolePermissionService],
   controllers: [RoleNewController, RolePermissionNewController],
