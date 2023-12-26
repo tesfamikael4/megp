5837672bd34f04557ba530d7f29b0c58f83ee66d
ermilla <wteffera11@gmail.com>
2023-12-26 17:11:46 +0300

    feat(vendor-fe): renewal and upgrade with random color for company names

---
+++ apps/back-office/vendor-fe/src/app/(features)/_components/application-list/index.tsx
@@ -0,0 +1,200 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import {
+  Box,
+  Text,
+  Flex,
+  Skeleton,
+  Avatar,
+  Tooltip,
+  Group,
+  Pagination,
+} from '@mantine/core';
+import {
+  IconTicket,
+  IconFileInvoice,
+  IconTag,
+  IconClockHour2,
+} from '@tabler/icons-react';
+import { Section } from '@megp/core-fe';
+import {
+  useGetAllVendorRequestsQuery,
+  useLazyGetAllVendorRequestsQuery,
+} from '@/store/api/vendor_request_handler/new-registration-api';
+import { useRouter } from 'next/navigation';
+import RequestsSidebar from '../requests-sidebar';
+import { processCompanyName } from '../../util';
+
+const perPage = 15;
+
+function calculateTotalPages(totalItems: number, itemsPerPage: number): number {
+  if (totalItems <= 0 || itemsPerPage <= 0) {
+    return 0;
+  }
+
+  return Math.ceil(totalItems / itemsPerPage);
+}
+
+type ApplicationListProp = {
+  serviceKey: 'new' | 'renewal' | 'upgrade';
+  title: string;
+};
+
+export default function ApplicationList({
+  serviceKey,
+  title,
+}: ApplicationListProp) {
+  const router = useRouter();
+  const [getVendorRequest, { data }] = useLazyGetAllVendorRequestsQuery();
+  const [page, setPage] = useState(1);
+  const totalPages = calculateTotalPages(data?.total || 0, perPage);
+
+  useEffect(() => {
+    getVendorRequest({
+      serviceKey,
+      collectionQuery: {
+        take: 15,
+        skip: 0,
+      },
+    });
+  }, [serviceKey]);
+
+  useEffect(() => {
+    const from = (page - 1) * perPage;
+
+    getVendorRequest?.({
+      serviceKey,
+      collectionQuery: {
+        skip: from,
+        take: perPage,
+      },
+    });
+  }, [page, serviceKey]);
+
+  if (!data) {
+    return (
+      <>
+        <Box className="p-5 w-10/12">
+          <Skeleton height={50} circle mb="xl" />
+          <Skeleton height={8} radius="xl" />
+          <Skeleton height={8} mt={10} radius="xl" />
+          <Skeleton height={8} mt={10} width="70%" radius="xl" />
+
+          <Skeleton height={50} mt={10} circle mb="xl" />
+          <Skeleton height={8} radius="xl" />
+          <Skeleton height={8} mt={10} radius="xl" />
+          <Skeleton height={8} mt={10} width="70%" radius="xl" />
+
+          <Skeleton height={50} mt={10} circle mb="xl" />
+          <Skeleton height={8} radius="xl" />
+          <Skeleton height={8} mt={10} radius="xl" />
+          <Skeleton height={8} mt={10} width="70%" radius="xl" />
+
+          <Skeleton height={50} mt={10} circle mb="xl" />
+          <Skeleton height={8} radius="xl" />
+          <Skeleton height={8} mt={10} radius="xl" />
+          <Skeleton height={8} mt={10} width="70%" radius="xl" />
+        </Box>
+      </>
+    );
+  }
+
+  return (
+    <Flex direction="row" gap="sm">
+      <Section collapsible={false} title={`Vendor ${title} Applications`}>
+        {data?.total === 0 && (
+          <Box
+            h={500}
+            className="w-full h-full flex items-center justify-center"
+          >
+            <Text className="text-gray-500 text-xl">
+              No {title} applications found
+            </Text>
+          </Box>
+        )}
+        {data?.items.map((item, index) => {
+          const { initials, color } = processCompanyName(
+            item.isrvendor?.basic.name,
+          );
+          return (
+            <Box
+              key={index}
+              onClick={() => router.push(`/new/detail/${item.id}`)}
+            >
+              <Flex
+                direction="row"
+                justify="space-between"
+                className={`items-center border border-gray-200 p-3 cursor-pointer hover:bg-primary-50 ${
+                  index % 2 === 0 ? 'bg-gray-100' : ''
+                }`}
+              >
+                <Box className="pr-3">
+                  <Avatar color={'white'} radius="xl" size="lg" bg={color}>
+                    {initials}
+                  </Avatar>
+                </Box>
+                <Box className="gap-[0.125rem] w-2/3">
+                  <Box className="text-sm">
+                    <Box className="text-primary-800 font-bold">
+                      {item.isrvendor?.basic.name}
+                    </Box>
+                    <Box>
+                      <span className="font-semibold">Country:</span>{' '}
+                      {item.isrvendor?.basic.origin}
+                    </Box>
+                    <Box>
+                      <span className="font-semibold">Current Task:</span>{' '}
+                      {item.task?.name}
+                    </Box>
+                  </Box>
+                </Box>
+                <Flex
+                  direction="column"
+                  className="gap-[0.125rem] border-l border-gray-200 w-1/3 pl-2"
+                >
+                  <Flex direction="row" className="items-center gap-1 text-sm">
+                    <Tooltip label="Tracking number">
+                      <IconTicket size={18} />
+                    </Tooltip>
+                    <Box>{item.applicationNumber}</Box>
+                  </Flex>
+                  <Flex direction="row" className="items-center gap-1 text-sm">
+                    <Tooltip label="Business area">
+                      <IconFileInvoice size={18} />
+                    </Tooltip>
+                    <Box>{item.service.name}</Box>
+                  </Flex>
+
+                  <Flex direction="row" className="items-center  gap-1 text-sm">
+                    <Tooltip label="Executed time">
+                      <IconClockHour2 size={18} />
+                    </Tooltip>
+                    <Box>{item.submittedAt.split('T')[0]}</Box>
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Box>
+          );
+        })}
+        {data.total > 0 ? (
+          <Group justify="space-between" mb="lg" mt="lg">
+            {data.total !== 0 && (
+              <Text>Total : {data.total.toLocaleString()} results</Text>
+            )}
+
+            <Pagination
+              onChange={setPage}
+              size="sm"
+              total={totalPages}
+              value={page}
+              withEdges
+            />
+          </Group>
+        ) : null}
+      </Section>
+      <Box className="w-2/6">
+        <RequestsSidebar />
+      </Box>
+    </Flex>
+  );
+}
--- apps/back-office/vendor-fe/src/store/api/vendor_request_handler/new-registration-api.ts
+++ apps/back-office/vendor-fe/src/store/api/vendor_request_handler/new-registration-api.ts
@@ -2,56 +2,73 @@ import { VendorRequests, Data } from './type';
 import { VendorRequest } from './type';
 import { createApi } from '@reduxjs/toolkit/dist/query/react';
 import { baseQuery } from '@/store/base-query';
+import { CollectionQuery, encodeCollectionQuery } from '@megp/entity';
 
 export const newRegistrationSlice = createApi({
   reducerPath: 'vendorRequestApi',
   refetchOnFocus: true,
   tagTypes: ['Tasks'],
-  baseQuery: baseQuery(process.env.NEXT_PUBLIC_VENDOR_API ?? '/vendors'),
+  baseQuery: baseQuery(process.env.NEXT_PUBLIC_VENDOR_API ?? '/vendors/api/'),
   endpoints: (builder) => ({
     getApplicationRequestDetailById: builder.query<any, VendorRequest>({
       query: (payload) => ({
-        url: `api/application-execution/get-currunt-task-detail/${payload.instanceId}`,
+        url: `application-execution/get-currunt-task-detail/${payload.instanceId}`,
         method: 'GET',
       }),
       providesTags: ['Tasks'],
     }),
     goToNextState: builder.mutation<any, any>({
       query: (data) => ({
-        url: `api/application-execution/goto-next-step`,
+        url: `application-execution/goto-next-step`,
         method: 'POST',
         body: data,
       }),
       invalidatesTags: ['Tasks'],
     }),
-    getAllVendorRequests: builder.query<Data, VendorRequests>({
-      query: (payload) => ({
-        url: `api/application-execution/get-currunt-tasks/${payload.serviceKey}`,
-        method: 'GET',
-      }),
+    getAllVendorRequests: builder.query<
+      Data,
+      VendorRequests & { collectionQuery: CollectionQuery | undefined }
+    >({
+      query: ({ serviceKey, collectionQuery }) => {
+        let q = '';
+        if (collectionQuery) {
+          const query = encodeCollectionQuery(collectionQuery);
+          q = `?q=${query}`;
+
+          return {
+            url: `application-execution/get-currunt-tasks/${serviceKey}${q}`,
+            method: 'GET',
+          };
+        } else {
+          return {
+            url: `application-execution/get-currunt-tasks/${serviceKey}`,
+            method: 'GET',
+          };
+        }
+      },
       providesTags: ['Tasks'],
     }),
     getVendor: builder.query<any, { id: string }>({
       query: (payload) => ({
-        url: `api/vendor-registrations/get-vendor-by-vendorId/${payload.id}`,
+        url: `vendor-registrations/get-vendor-by-vendorId/${payload.id}`,
       }),
     }),
     getInvoice: builder.query({
       query: () => ({
-        url: `api/application-execution/get-invoices`,
+        url: `application-execution/get-invoices`,
         method: 'GET',
       }),
     }),
     pickTask: builder.mutation<any, any>({
       query: (data) => ({
-        url: `api/application-execution/pick-task`,
+        url: `application-execution/pick-task`,
         method: 'POST',
         body: data,
       }),
     }),
     unpickTask: builder.mutation<any, any>({
       query: (data) => ({
-        url: `api/application-execution/unpick-task`,
+        url: `application-execution/unpick-task`,
         method: 'POST',
         body: data,
       }),
@@ -61,7 +78,7 @@ export const newRegistrationSlice = createApi({
       { vendorId: string; instanceId: string }
     >({
       query: (payload) => ({
-        url: `api/certificates/generate-certeficate/${payload.vendorId}/${payload.instanceId}`,
+        url: `certificates/generate-certeficate/${payload.vendorId}/${payload.instanceId}`,
         method: 'GET',
       }),
     }),
@@ -71,6 +88,7 @@ export const newRegistrationSlice = createApi({
 export const {
   useGetAllVendorRequestsQuery,
   useGetApplicationRequestDetailByIdQuery,
+  useLazyGetAllVendorRequestsQuery,
   useGetInvoiceQuery,
   useGoToNextStateMutation,
   usePickTaskMutation,
