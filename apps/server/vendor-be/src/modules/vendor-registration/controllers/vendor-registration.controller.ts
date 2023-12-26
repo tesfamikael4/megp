21c0f471cbfad11821e36c27855bad7751714d40
Yaregal Tessema <yayasoles@gmail.com>
2023-12-26 14:48:24 +0000

    Vendor service renewal upgrade

--- apps/server/vendor-be/src/modules/vendor-registration/controllers/vendor-registration.controller.ts
+++ apps/server/vendor-be/src/modules/vendor-registration/controllers/vendor-registration.controller.ts
@@ -35,7 +35,7 @@ import { CollectionQuery } from 'src/shared/collection-query';
 @ApiResponse({ status: 500, description: 'Internal error' })
 @ApiExtraModels(DataResponseFormat)
 export class VendorRegistrationsController {
-  constructor(private readonly regService: VendorRegistrationsService) {}
+  constructor(private readonly regService: VendorRegistrationsService) { }
   @UseGuards(JwtGuard)
   @Get('get-isr-vendors')
   async getVendors() {
