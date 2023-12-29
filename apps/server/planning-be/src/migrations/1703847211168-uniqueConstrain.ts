6fbb0fb266c3c19806b47223440a7507b7b40c21
Natnael Girma <nathygirma2524@gmail.com>
2023-12-29 11:12:30 +0000

    Workflow ver

--- apps/server/planning-be/src/migrations/1703847211168-uniqueConstrain.ts
+++ apps/server/planning-be/src/migrations/1703847211168-uniqueConstrain.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class BudgerYearRelation1703840163660 implements MigrationInterface {
-  name = 'BudgerYearRelation1703840163660';
+export class UniqueConstrain1703847211168 implements MigrationInterface {
+  name = 'UniqueConstrain1703847211168';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
@@ -32,12 +32,6 @@ export class BudgerYearRelation1703840163660 implements MigrationInterface {
       `ALTER TABLE "item_budget_line" ALTER COLUMN "activityBudgetLineId" SET NOT NULL`,
     );
     await queryRunner.query(
-      `ALTER TABLE "apps" DROP CONSTRAINT "FK_d855a79a1fd5aff929051e04b6a"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "apps" DROP CONSTRAINT "UQ_d855a79a1fd5aff929051e04b6a"`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "post_budget_plan_activities" ADD CONSTRAINT "UQ_ad1143400a9a09b9b8d52985f1f" UNIQUE ("procurementReference")`,
     );
     await queryRunner.query(
@@ -51,6 +45,12 @@ export class BudgerYearRelation1703840163660 implements MigrationInterface {
     );
     await queryRunner.query(
       `ALTER TABLE "post_budget_plan_activities" ADD "calculatedAmount" bigint NOT NULL DEFAULT '0'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "apps" DROP CONSTRAINT "FK_d855a79a1fd5aff929051e04b6a"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "apps" DROP CONSTRAINT "UQ_d855a79a1fd5aff929051e04b6a"`,
     );
     await queryRunner.query(
       `ALTER TABLE "item_budget_line" ADD CONSTRAINT "FK_3be6aa33082620f9ffae4fd6098" FOREIGN KEY ("activityBudgetLineId") REFERENCES "activity_budget_lines"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
@@ -68,6 +68,12 @@ export class BudgerYearRelation1703840163660 implements MigrationInterface {
       `ALTER TABLE "item_budget_line" DROP CONSTRAINT "FK_3be6aa33082620f9ffae4fd6098"`,
     );
     await queryRunner.query(
+      `ALTER TABLE "apps" ADD CONSTRAINT "UQ_d855a79a1fd5aff929051e04b6a" UNIQUE ("budgetYearId")`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "apps" ADD CONSTRAINT "FK_d855a79a1fd5aff929051e04b6a" FOREIGN KEY ("budgetYearId") REFERENCES "budget_years"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
       `ALTER TABLE "post_budget_plan_activities" DROP COLUMN "calculatedAmount"`,
     );
     await queryRunner.query(
@@ -81,12 +87,6 @@ export class BudgerYearRelation1703840163660 implements MigrationInterface {
     );
     await queryRunner.query(
       `ALTER TABLE "post_budget_plan_activities" DROP CONSTRAINT "UQ_ad1143400a9a09b9b8d52985f1f"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "apps" ADD CONSTRAINT "UQ_d855a79a1fd5aff929051e04b6a" UNIQUE ("budgetYearId")`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "apps" ADD CONSTRAINT "FK_d855a79a1fd5aff929051e04b6a" FOREIGN KEY ("budgetYearId") REFERENCES "budget_years"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
       `ALTER TABLE "item_budget_line" ALTER COLUMN "activityBudgetLineId" DROP NOT NULL`,
