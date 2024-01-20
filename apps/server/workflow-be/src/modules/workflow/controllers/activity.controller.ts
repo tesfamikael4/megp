fd2c22d3f907e0e5ed8b43faa37f763012682615
Natnael Girma <nathygirma2524@gmail.com>
2024-01-20 15:16:59 +0000

    Approval fix

--- apps/server/workflow-be/src/modules/workflow/controllers/activity.controller.ts
+++ apps/server/workflow-be/src/modules/workflow/controllers/activity.controller.ts
@@ -1,5 +1,5 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import {
   EntityCrudOptions,
   ExtraCrudOptions,
@@ -12,6 +12,8 @@ import { Step } from 'src/entities/step.entity';
 import { StepService } from '../services/step.service';
 import { Activity } from 'src/entities/activity.entity';
 import { ActivityService } from '../services/activity.service';
+import { decodeCollectionQuery } from 'src/shared/collection-query';
+import { IgnoreTenantInterceptor } from 'src/shared/decorators';
 
 const options: EntityCrudOptions = {};
 
@@ -22,5 +24,23 @@ export class ActivityController extends EntityCrudController<Activity>(
 ) {
   constructor(private readonly activityService: ActivityService) {
     super(activityService);
+  }
+  @Get()
+  @ApiQuery({
+    name: 'q',
+    type: String,
+    description: 'Collection Query Parameter. Optional',
+    required: false,
+  })
+  @IgnoreTenantInterceptor()
+  async findAll(@Query('q') q?: string, @Req() req?: any) {
+    const query = decodeCollectionQuery(q);
+    return this.activityService.findAll(query);
+  }
+
+  @Get(':id')
+  @IgnoreTenantInterceptor()
+  async findOne(@Param('id') id: string, @Req() req?: any) {
+    return this.activityService.findOne(id);
   }
 }
