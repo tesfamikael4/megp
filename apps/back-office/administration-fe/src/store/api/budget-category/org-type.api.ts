26868680a2f17e3956dcee264facd20c024b401a
kidest legesse <adeyhaile09@gmail.com>
2024-01-25 15:16:10 +0300

    fix(administration-fe): fixed org type

--- apps/back-office/administration-fe/src/store/api/budget-category/org-type.api.ts
+++ apps/back-office/administration-fe/src/store/api/budget-category/org-type.api.ts
@@ -1,11 +1,10 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { baseQuery } from '@/store/base-query';
+import { createApi } from '@reduxjs/toolkit/query/react';
 
 export const budgeCategoryApi = createApi({
   reducerPath: 'budegeCategoryApi',
   refetchOnFocus: true,
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_IAM_API ?? '/iam/api/',
-  }),
+  baseQuery: baseQuery(process.env.NEXT_PUBLIC_IAM_API ?? '/iam/api/'),
   endpoints: (builder) => ({
     getOrgType: builder.query<any, { id: string }>({
       query: ({ id }) => ({
