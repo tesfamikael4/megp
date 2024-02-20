3f2470c23c5be38161377208eaf8ffdea17fd359
ermilla <wteffera11@gmail.com>
2024-02-20 12:19:14 +0300

    fix(portal): mobile ui

--- apps/portal/egp/src/app/(features)/vendor/(workspace)/layout.tsx
+++ apps/portal/egp/src/app/(features)/vendor/(workspace)/layout.tsx
@@ -3,20 +3,22 @@ import { Flex } from '@mantine/core';
 import { useState } from 'react';
 import styles from './layout.module.scss';
 import Sidebar from '../_components/sidebar/sidebar';
+import Protected from '../../protected';
 interface Props {
   children: React.ReactNode;
 }
-
 export default function WorkspaceLayout({ children }: Props) {
   const [isSidebarOpen] = useState(false);
 
   return (
-    <Flex>
-      <nav data-open={isSidebarOpen} className={styles.nav}>
-        {' '}
-        <Sidebar />{' '}
-      </nav>
-      <main className={styles.main}>{children}</main>
-    </Flex>
+    <Protected>
+      <Flex>
+        <nav data-open={isSidebarOpen} className={styles.nav}>
+          {' '}
+          <Sidebar />{' '}
+        </nav>
+        <main className={styles.main}>{children}</main>
+      </Flex>
+    </Protected>
   );
 }
