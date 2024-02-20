bf617670d9943348eab25de560e7ddf0916fbf86
Dagwal <108694236+Dagwal@users.noreply.github.com>
2024-02-20 07:05:53 +0300

    fix(portal): removed lodash package and refactor some ui changes like dropdown

--- apps/portal/egp/src/app/(features)/vendor/registration/my-payments/_components/table.tsx
+++ apps/portal/egp/src/app/(features)/vendor/registration/my-payments/_components/table.tsx
@@ -1,28 +1,10 @@
 'use client';
 
-import {
-  ActionIcon,
-  Box,
-  Button,
-  Grid,
-  GridCol,
-  Group,
-  Stack,
-  Text,
-} from '@mantine/core';
-import { closeModal, openModal } from '@mantine/modals';
-import { IconDotsVertical } from '@tabler/icons-react';
+import { ActionIcon, Box, Menu, rem } from '@mantine/core';
+import { IconDotsVertical, IconEdit, IconTrash } from '@tabler/icons-react';
 import { DataTable } from 'mantine-datatable';
-
+import classes from './table.module.scss';
 const companies = [
-  {
-    id: '1323addd-a4ac-4dd2-8de2-6f934969a0f1',
-    name: 'Feest, Bogan and Herzog',
-    streetAddress: '21716 Ratke Drive',
-    city: 'Stromanport',
-    state: 'WY',
-    missionStatement: 'Innovate bricks-and-clicks metrics.',
-  },
   {
     id: '1323addd-a4ac-4dd2-8de2-6f934969a0f1',
     name: 'Feest, Bogan and Herzog',
@@ -60,38 +42,6 @@ export type Company = {
   missionStatement: string;
 };
 
-const showModal = ({
-  company,
-  action,
-}: {
-  company: Company;
-  action: string;
-}) => {
-  openModal({
-    title: 'Row action',
-    children: (
-      <Stack>
-        <Text>
-          <Text w={700}>{company.name}</Text>
-        </Text>
-        <Grid>
-          <GridCol span={6}>
-            <Button
-              fullWidth
-              onClick={() => closeModal('close')}
-              variant="outline"
-              color="gray"
-            >
-              Close
-            </Button>
-          </GridCol>
-        </Grid>
-      </Stack>
-    ),
-    size: 'sm',
-  });
-};
-
 export default function Table() {
   return (
     <DataTable
@@ -106,18 +56,33 @@ export default function Table() {
           accessor: 'actions',
           title: <Box mr={6}>Row actions</Box>,
           textAlign: 'right',
-          render: (company) => (
-            <Group gap={4} justify="right" wrap="nowrap">
-              <ActionIcon
-                size="sm"
-                variant="subtle"
-                color="green"
-                // ml={'md'}
-                onClick={() => showModal({ company, action: 'view' })}
-              >
-                <IconDotsVertical size={16} />
-              </ActionIcon>
-            </Group>
+          render: () => (
+            <Menu shadow="md" width={100}>
+              <Menu.Target>
+                <ActionIcon right={32} size="sm" variant="subtle" color="green">
+                  <IconDotsVertical size={16} />
+                </ActionIcon>
+              </Menu.Target>
+
+              <Menu.Dropdown>
+                <Menu.Divider />
+                <Menu.Item
+                  leftSection={
+                    <IconEdit style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Edit
+                </Menu.Item>
+                <Menu.Item
+                  color="red"
+                  leftSection={
+                    <IconTrash style={{ width: rem(14), height: rem(14) }} />
+                  }
+                >
+                  Delete
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
           ),
         },
       ]}
@@ -127,6 +92,7 @@ export default function Table() {
           backgroundColor: '#E8E8E8',
         },
       }}
+      classNames={{ table: classes.table }}
     />
   );
 }
