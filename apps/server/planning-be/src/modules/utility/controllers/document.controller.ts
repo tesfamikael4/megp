5902be31e8577d9c78880940507ffcc9f3f58574
Natnael Girma <nathygirma2524@gmail.com>
2024-03-05 11:49:01 +0000

    Report pdf

---
+++ apps/server/planning-be/src/modules/utility/controllers/document.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { EntityCrudOptions } from 'src/shared/types/crud-option.type';
+import { EntityCrudController } from 'src/shared/controller';
+import { CurrentUser } from 'src/shared/authorization';
+import { DocumentService } from '../services/document.service';
+import { Document } from 'src/entities/document.entity';
+
+const options: EntityCrudOptions = {};
+
+@Controller('documents')
+@ApiTags('documents')
+export class DocumentController extends EntityCrudController<Document>(
+  options,
+) {
+  constructor(private readonly documentService: DocumentService) {
+    super(documentService);
+  }
+
+  @Get('download/:id')
+  async download(@Param('id') id: string) {
+    const presignedUrl = await this.documentService.generatePresignedGetUrl(id);
+    return presignedUrl;
+  }
+
+  @Get('getDocumentByItemId/:id')
+  async getDocumentByItemId(@Param('id') id: string) {
+    const document = await this.documentService.getDocumentByItemId(id);
+    return document;
+  }
+}
