4b087c1e79ae71fb143861a9c9fe48e5b1c36d93
ermilla <wteffera11@gmail.com>
2024-03-08 14:47:53 +0300

    refactor(portal): vendor registration api refactor

---
+++ apps/portal/egp/src/app/(features)/vendor/(workspace)/registration/_api/vendor-regitration-query.api.ts
@@ -0,0 +1,116 @@
+import {
+  AddFormRequest,
+  AreasOfBusinessInterestType,
+  CreateVendorIdRequest,
+  CreateVendorIdResponse,
+  GetCertificateInformationResponse,
+  GetForRenewalVendorResponse,
+  GetFormResponse,
+  GetVendorInfoResponse,
+  RenewalInvoiceRenewalVendorResponse,
+} from '@/models/vendorRegistration';
+import { vendorRegistrationApi } from '@/store/api/vendor_registration/api';
+
+export const vendorRegistrationQuery = vendorRegistrationApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getVendor: builder.query<GetFormResponse, { flag?: string }>({
+      query: ({ flag }) => {
+        return {
+          url: `/vendor-registrations/get-isr-vendor-by-userId`,
+          params: { flag },
+        };
+      },
+    }),
+    getVendorOnDemand: builder.query<GetFormResponse, any>({
+      query: () => `/vendor-registrations/get-isr-vendor-by-userId`,
+    }),
+    getVendorInfo: builder.query<GetVendorInfoResponse, any>({
+      query: () => `/vendor-registrations/get-isr-vendor-info-by-userId`,
+    }),
+    getApprovedVendorInfo: builder.query<GetVendorInfoResponse, any>({
+      query: () =>
+        `/vendor-registrations/get-approved-isr-vendor-info-by-userId`,
+    }),
+    getApproveVendorInfo: builder.query<GetFormResponse, any>({
+      query: () => `/vendor-registrations/get-vendor-information`,
+    }),
+    getForm: builder.query<GetFormResponse, any>({
+      query: () => `/vendor-registrations/get-vendor-by-vendorId`,
+    }),
+    getInvoice: builder.query<GetFormResponse, any>({
+      query: () => `/vendor-registrations/get-isr-vendor-invoice-by-userId`,
+    }),
+    getForRenewalVendor: builder.query<GetForRenewalVendorResponse, any>({
+      query: () => `/vendor-registrations/get-approved-vendor-service-byUserId`,
+    }),
+    postRenewalVendor: builder.query<GetForRenewalVendorResponse, string[]>({
+      query: (data) => ({
+        url: `/vendor-registrations/submit-service-renewal`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+
+    getInvoiceByUserId: builder.query<RenewalInvoiceRenewalVendorResponse, any>(
+      {
+        query: () => ({
+          url: `/vendor-registrations/get-invoice-by-userId`,
+          method: 'GET',
+          // body: data,
+        }),
+      },
+    ),
+    getVendorStatus: builder.query<
+      {
+        status: 'Submitted' | 'Draft' | 'Initial' | 'Completed' | 'Approved';
+        initial: { status: string; level: string };
+      },
+      any
+    >({
+      query: () => ({
+        url: `/vendor-registrations/get-application-status-by-userId`,
+      }),
+    }),
+    getPendingApplications: builder.query<any, any>({
+      query: () => ({
+        url: `/vendor-registrations/get-pending-vendor-applications`,
+      }),
+    }),
+    createVendorId: builder.mutation<
+      CreateVendorIdResponse,
+      CreateVendorIdRequest
+    >({
+      query: (data) => ({
+        url: '/vendor-registrations/vendor-initiation',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    addForm: builder.mutation<any, AddFormRequest>({
+      query: (newany) => ({
+        url: '/vendor-registrations/add-vendor-information',
+        method: 'POST',
+        body: newany,
+      }),
+    }),
+
+    getCertificateInformation: builder.query<
+      GetCertificateInformationResponse,
+      void
+    >({
+      query() {
+        return {
+          url: `vendor-registrations/get-certificate-informations`,
+          method: 'GET',
+        };
+      },
+    }),
+    addAdditionalService: builder.mutation<any, AreasOfBusinessInterestType[]>({
+      query: (data) => ({
+        url: `vendor-registrations/add-service`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
+});
