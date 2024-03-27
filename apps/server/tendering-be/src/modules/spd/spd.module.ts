5e9285999b4da8d5e1256cb8599a3ad82552eebf
zolaDO <38725248+zola0031@users.noreply.github.com>
2024-03-27 21:22:14 +0300

    fix(iam-be): spd opening checklist added on module

--- apps/server/tendering-be/src/modules/spd/spd.module.ts
+++ apps/server/tendering-be/src/modules/spd/spd.module.ts
@@ -4,6 +4,7 @@ import { DocxModule } from 'src/shared/docx/docx.module';
 import { MinIOModule } from 'src/shared/min-io/min-io.module';
 import {
   Spd,
+  SpdOpeningChecklist,
   SpdPreferenceMargin,
   SpdPreliminaryEvaluation,
   SpdProfessionalSetting,
@@ -19,6 +20,7 @@ import {
   SpdQualificationService,
   SpdTechnicalScoringService,
   SpdTemplateService,
+  SpdOpeningChecklistService,
 } from './service';
 import {
   SpdController,
@@ -37,6 +39,7 @@ import { SpdContractForm } from 'src/entities/spd-contract-form.entity';
 import { SpdContractFormService } from './service/spd-contract-form.service';
 import { SpdContractFormController } from './controller/spd-contract-form.controller';
 import { DocumentManipulatorModule } from 'src/shared/document-manipulator/document-manipulator.module';
+import { SpdOpeningChecklistController } from './controller/spd-opening-checklist.controller';
 
 @Module({
   imports: [
@@ -50,6 +53,7 @@ import { DocumentManipulatorModule } from 'src/shared/document-manipulator/docum
       SpdTemplate,
       SpdBidForm,
       SpdContractForm,
+      SpdOpeningChecklist,
     ]),
     DocxModule,
     MinIOModule,
@@ -66,6 +70,7 @@ import { DocumentManipulatorModule } from 'src/shared/document-manipulator/docum
     FileHelperService,
     SpdBidFormService,
     SpdContractFormService,
+    SpdOpeningChecklistService,
   ],
   controllers: [
     SpdController,
@@ -77,6 +82,7 @@ import { DocumentManipulatorModule } from 'src/shared/document-manipulator/docum
     SpdTemplateController,
     SpdBidFormController,
     SpdContractFormController,
+    SpdOpeningChecklistController,
   ],
 })
 export class SpdModule {}
