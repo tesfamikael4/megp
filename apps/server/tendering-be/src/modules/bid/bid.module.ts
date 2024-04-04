3fa5c6d7f2467dc78e696336f04006062b9de42c
zolaDO <38725248+zola0031@users.noreply.github.com>
2024-04-04 15:59:37 +0300

    feat(tendering-be): bid submission addded

--- apps/server/tendering-be/src/modules/bid/bid.module.ts
+++ apps/server/tendering-be/src/modules/bid/bid.module.ts
@@ -4,6 +4,7 @@ import { BidBookmark } from 'src/entities/bid-bookmark.entity';
 import { BidBookmarkController } from './controller/bid-bookmark.controller';
 import { BidBookmarkService } from './service/bid-bookmark.service';
 import { BidRegistration } from 'src/entities/bid-registration.entity';
+import { BidRegistrationDetail } from 'src/entities/bid-registration-detail.entity';
 import { BidRegistrationService } from './service/bid-registration.service';
 import { BidRegistrationController } from './controller/bid-registration.controller';
 import { BidResponse } from 'src/entities/bid-response.entity';
@@ -11,7 +12,6 @@ import { BidResponseService } from './service/bid-response.service';
 import { BidResponseController } from './controller/bid-response.controller';
 import { EncryptionHelperService } from './service/encryption-helper.service';
 import { AuthorizationModule } from 'src/shared/authorization';
-import { BidRegistrationDetail } from 'src/entities/bid-registration-detail.entity';
 
 @Module({
   imports: [
