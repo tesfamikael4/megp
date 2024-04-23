bc98cc9b78c223bc6501b691f79076c75372476a
Misikir <misikir.adane@gmail.com>
2024-04-24 00:23:39 +0300

    ci: rebuild

--- apps/portal/egp/src/app/(features)/about-us/page.tsx
+++ apps/portal/egp/src/app/(features)/about-us/page.tsx
@@ -43,7 +43,7 @@ const Page = () => {
       icon: IconUser,
     },
     {
-      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+      title: 'Lorem ipsum dolor sit amet,  adipiscing',
       description:
         'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       icon: IconUser,
