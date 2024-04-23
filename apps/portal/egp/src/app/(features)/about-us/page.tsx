64c5b2a94bff17d0256fc2d6c1a7a70a8c55b764
Misikir <misikir.adane@gmail.com>
2024-04-24 00:12:30 +0300

    ci: trigger

--- apps/portal/egp/src/app/(features)/about-us/page.tsx
+++ apps/portal/egp/src/app/(features)/about-us/page.tsx
@@ -39,7 +39,7 @@ const Page = () => {
     {
       title: 'Lorem ipsum dolor sit , consectetur adipiscing',
       description:
-        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Duis aute irure dolor in  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       icon: IconUser,
     },
     {
