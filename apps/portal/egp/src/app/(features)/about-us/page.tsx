c86c3278538907b73062b2be6fcbb5e284b24c9b
Misikir <misikir.adane@gmail.com>
2024-04-26 01:23:42 +0300

    ci: build only egp

--- apps/portal/egp/src/app/(features)/about-us/page.tsx
+++ apps/portal/egp/src/app/(features)/about-us/page.tsx
@@ -45,7 +45,7 @@ const Page = () => {
     {
       title: 'Lorem ipsum dolor sit amet,  adipiscing',
       description:
-        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Duis aute irure dolor in reprehenderit in  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       icon: IconUser,
     },
     {
