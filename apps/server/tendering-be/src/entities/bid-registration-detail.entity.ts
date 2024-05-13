df08848e8bc9092fbddaaff21d5b98825a96b7bc
Natnael Girma <nathygirma2524@gmail.com>
2024-05-13 17:03:21 +0300

    feat(tendering-be): bidders comparison

--- apps/server/tendering-be/src/entities/bid-registration-detail.entity.ts
+++ apps/server/tendering-be/src/entities/bid-registration-detail.entity.ts
@@ -16,8 +16,8 @@ import { BidResponseLot } from './bid-response-lot.entity';
 import { BidResponseItem } from './bid-response-item.entity';
 import { OpenedBidResponseItem } from './opened-bid-response-item.entity';
 import { OpenedBidResponseLot } from './opened-bid-response-lot.entity';
-import { TechnicalPreliminaryAssessmentDetail } from './technical-preliminary-assessment-detail.entity';
 import { TechnicalPreliminaryAssessment } from './technical-preliminary-assessment.entity';
+import { BiddersComparison } from './bidders-comparison.entity';
 
 @Entity({ name: 'bid_registration_details' })
 @Unique(['bidRegistrationId', 'lotId'])
@@ -81,6 +81,11 @@ export class BidRegistrationDetail extends Audit {
     (technicalPreliminaryAssessment) =>
       technicalPreliminaryAssessment.bidRegistrationDetail,
   )
-  @JoinColumn()
   technicalPreliminaryAssessment: TechnicalPreliminaryAssessment;
+
+  @OneToMany(
+    () => BiddersComparison,
+    (biddersComparison) => biddersComparison.bidRegistrationDetails,
+  )
+  biddersComparison: BiddersComparison;
 }
---
+++ apps/server/tendering-be/src/modules/bidders/controller/bidders-comparison.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { BiddersComparison } from 'src/entities/bidders-comparison.entity';
+
+import { BiddersComparisonService } from '../services/bidders-comparison.services';
+import { ExtraCrudOptions } from 'src/shared/types/crud-option.type';
+import { ExtraCrudController } from 'src/shared/controller';
+
+const options: ExtraCrudOptions = {
+  entityIdName: 'bidRegistrationId',
+};
+
+@Controller('bidders-comparison')
+@ApiTags('Bidders Comparison Controller')
+export class BiddersComparisonController extends ExtraCrudController<BiddersComparison>(
+  options,
+) {
+  constructor(
+    private readonly biddersComparisonService: BiddersComparisonService,
+  ) {
+    super(biddersComparisonService);
+  }
+}
