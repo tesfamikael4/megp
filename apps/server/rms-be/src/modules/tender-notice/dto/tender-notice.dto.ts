93694ba7e3d96b6fad81ee5dd124f4151964f8ea
BereketeAb Birku <berekete.abb12@gmail.com>
2024-05-30 13:04:41 +0000

    feat(rms): init

--- apps/server/rms-be/src/modules/tender-notice/dto/tender-notice.dto.ts
+++ apps/server/rms-be/src/modules/tender-notice/dto/tender-notice.dto.ts
@@ -1,11 +1,78 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+import { ETenderNoticeType } from 'src/utils/enums/tender-notice.enum';
 
 export class CreateTenderNoticeDto {
   @ApiProperty()
   @IsNotEmpty()
-  title: string;
+  objectId: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEnum(ETenderNoticeType)
+  object: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  procurementCategory: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  procurementReferenceNumber: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  budgetCode: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  prId: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsObject()
+  metadata: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  organizationId: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  organizationName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  publishmentDate: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  closingDate: string;
 }
+
 export class UpdateTenderNoticeDto extends CreateTenderNoticeDto {
   @ApiProperty({
     format: 'uuid',
