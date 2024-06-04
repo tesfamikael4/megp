6ca6942c7c63567a6bba55ff045a3d4b1ccc8f64
zolaDO <38725248+zola0031@users.noreply.github.com>
2024-06-04 16:25:59 +0200

    feat(infra-be): updated on offline payment

--- apps/server/infrastructure-be/src/app.module.ts
+++ apps/server/infrastructure-be/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from 'megp-shared-be';
 import { AuthorizationModule } from 'megp-shared-be';
