84f4affd4bec59672399bf34c3ebb3714f6c4e81
zolaDO <38725248+zola0031@users.noreply.github.com>
2024-06-05 11:31:53 +0200

    feat(infra-be): updated on log controller

--- apps/server/administration-be/src/modules/budget-categories/budget-categories.module.ts
+++ apps/server/administration-be/src/modules/budget-categories/budget-categories.module.ts
@@ -3,10 +3,11 @@ import { BudgetCategoriesService } from './services/budget-categories.service';
 import { BudgetCategoriesController } from './controllers/budget-categories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BudgetCategory } from 'src/entities';
+import { LogController } from './controllers/log.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([BudgetCategory])],
-  controllers: [BudgetCategoriesController],
+  controllers: [BudgetCategoriesController, LogController],
   providers: [BudgetCategoriesService],
 })
 export class BudgetCategoriesModule {}
---
+++ apps/server/administration-be/src/modules/budget-categories/controllers/log.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { BudgetCategoriesService } from '../services/budget-categories.service';
+
+@Controller('log')
+export class LogController {
+  constructor(private readonly categoriesService: BudgetCategoriesService) {}
+
+  @Get('token')
+  async tokenLog(@Body() payload: any, @Req() req: Request) {
+    console.log(
+      '🚀 ~ OfflinePaymentController ~ authorization ~ req:',
+      req.headers.authorization,
+    );
+    console.log('🚀 ~ OfflinePaymentController ~ tokenLog ~ req:', req);
+    console.log('🚀 ~ OfflinePaymentController ~ log ~ payload:', payload);
+
+    return payload;
+  }
+}
