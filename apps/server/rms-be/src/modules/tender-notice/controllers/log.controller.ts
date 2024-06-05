3e32c900234c2687acc87b3e3175adeeaf3a0734
zolaDO <38725248+zola0031@users.noreply.github.com>
2024-06-05 22:59:23 +0200

    feat(rms-be): updated on rms build

--- apps/server/rms-be/src/modules/tender-notice/controllers/log.controller.ts
+++ apps/server/rms-be/src/modules/tender-notice/controllers/log.controller.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Get, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { TenderNoticeService } from '../services/tender-notice.service';
+import { ApiTags } from '@nestjs/swagger';
+import { AllowAnonymous } from 'megp-shared-be';
 
 @Controller('log')
+@ApiTags('Log')
 export class LogController {
   constructor(private readonly categoriesService: TenderNoticeService) {}
 
   @Get('token')
+  @AllowAnonymous()
   async tokenLog(@Body() payload: any, @Req() req: Request) {
     console.log(
       '🚀 ~ OfflinePaymentController ~ authorization ~ req:',
