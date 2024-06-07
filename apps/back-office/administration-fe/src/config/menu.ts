dca39258a5c22da5e177c576a2aaab1de6db9714
Lensa Belete <lensbelete@gmail.com>
2024-06-07 07:01:46 +0000

    fix(administration): edit the menu

--- apps/back-office/administration-fe/src/config/menu.ts
+++ apps/back-office/administration-fe/src/config/menu.ts
@@ -12,42 +12,56 @@ import {
   IconTable,
   IconTableShortcut,
   IconTools,
+  IconRegistered,
+  IconSearch,
 } from '@tabler/icons-react';
 
 export const Menu: MenuItem[] = [
-  { label: 'Organizations', icon: IconBuildingBank, link: '/organizations' },
-  { label: 'Taxonomy', icon: IconFileDollar, link: '/taxonomy' },
-  { label: 'Item Master', icon: IconTable, link: '/item-master' },
   {
-    label: 'Category',
-    icon: IconTableShortcut,
+    label: 'Item registry',
+    icon: IconRegistered,
     links: [
-      { label: 'Item Category', link: '/item-category' },
-      { label: 'Item Sub-Category', link: '/item-sub-category' },
+      { label: 'Taxonomy', link: '/taxonomy' },
+
+      { label: 'Item Master', link: '/item-master' },
+      {
+        label: 'Item Category',
+        link: '/item-category',
+      },
+      {
+        label: 'Item SubCategory',
+        link: '/item-sub-category',
+      },
+      { label: 'Item Tags', link: '/tag' },
+      { label: 'Measurements', link: '/measurement' },
+      {
+        label: 'Contract Catalogs',
+        link: '/contract-catalogs',
+      },
     ],
   },
+
   {
-    label: 'contract-catalogs',
-    icon: IconFile,
-    link: '/contract-catalogs',
+    label: 'Lookups',
+    icon: IconSearch,
+    links: [
+      { label: 'Donors', link: '/lookup/donors' },
+      { label: 'Region', link: '/region' },
+      { label: 'Currency', link: '/currency' },
+    ],
   },
-  { label: 'Measurements', icon: IconTools, link: '/measurement' },
-  { label: 'Item Tags', icon: IconBookmark, link: '/tag' },
-  { label: 'Rules', icon: IconFileCheck, link: '/rule-designer' },
-  { label: 'Currency', icon: IconDatabaseDollar, link: '/currency' },
-  { label: 'Tax', icon: IconFileDollar, link: '/tax' },
-  {
-    label: 'Target Group',
-    icon: IconBrandFlightradar24,
-    link: '/target-group',
-  },
-  { label: 'Region', icon: IconMapPin, link: '/region' },
   {
     label: 'Settings',
     icon: IconAdjustmentsHorizontal,
     links: [
-      { label: 'Budget Category', link: '/lookup/budget-category' },
-      { label: 'Donors', link: '/lookup/donors' },
+      { label: 'Procurement Rules', link: '/rule-designer' },
+      {
+        label: 'Target Group',
+        link: '/target-group',
+      },
+
+      // { label: 'Budget Category', link: '/lookup/budget-category' },
+      // { label: 'Donors', link: '/lookup/donors' },
       // { label: 'Procurement Institution', link: '/procurement-institution' },
       // { label: 'Procurement Method', link: '/lookup/procurement-method' },
       // {
@@ -65,4 +79,6 @@ export const Menu: MenuItem[] = [
       },
     ],
   },
+  // { label: 'Organizations', icon: IconBuildingBank, link: '/organizations' },
+  // { label: 'Tax', icon: IconFileDollar, link: '/tax' },
 ];
