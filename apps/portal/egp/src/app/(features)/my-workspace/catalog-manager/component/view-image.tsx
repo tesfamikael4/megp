77b328113209458b4b14dafbe54ce58c58c9e653
Samrawit Girma <samrawitgirma31@gmail.com>
2024-06-08 10:25:50 +0000

    Modify ui img

--- apps/portal/egp/src/app/(features)/my-workspace/catalog-manager/component/view-image.tsx
+++ apps/portal/egp/src/app/(features)/my-workspace/catalog-manager/component/view-image.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   useLazyGetFilesQuery,
   useLazyDownloadFilesQuery,
@@ -7,10 +7,15 @@ import {
 import { Box, Flex, Image, Text } from '@mantine/core';
 import { useParams } from 'next/navigation';
 import { UploadImage } from './image-upload';
-import { IconUpload } from '@tabler/icons-react';
+import {
+  IconUpload,
+  IconChevronRight,
+  IconChevronLeft,
+} from '@tabler/icons-react';
 
 export default function ViewImage() {
   const [getImage, { data, isLoading }] = useLazyGetFilesQuery();
+  const [index, setIndex] = useState(0);
   const { id } = useParams();
 
   const [dowloadPrFile, { data: url, isLoading: isPrLoading }] =
@@ -20,21 +25,68 @@ export default function ViewImage() {
     getImage(id?.toString());
   }, [getImage, id]);
 
+  const handleNext = () => {
+    if (index < (data?.items?.length ?? 0) - 1) {
+      setIndex((prevIndex) => prevIndex + 1);
+    } else {
+      setIndex(0);
+    }
+  };
+
+  const handlePrev = () => {
+    if (index > 0) {
+      setIndex((prevIndex) => prevIndex - 1);
+    } else {
+      setIndex((data?.items?.length ?? 0) - 1);
+    }
+  };
+
   useEffect(() => {
-    dowloadPrFile(data?.items?.[0]?.id);
-  }, [data]);
+    dowloadPrFile(data?.items?.[index]?.id);
+  }, [index, data]);
 
   return (
     <>
-      <Box className="w-full">
-        <Flex h={350} justify={'center'} align={'center'}>
+      <Box className="w-full ">
+        <Flex h={250} justify={'center'} align={'center'}>
           {!url && (
             <Flex align="center" direction={'column'}>
               <IconUpload size={30} color="#1D8E3F" />
               <Text>Upload Image</Text>
             </Flex>
           )}
-          {url && <Image src={url?.presignedUrl} alt="Product image" />}
+          {url && (
+            <Box className=" relative">
+              <Image
+                src={url?.presignedUrl}
+                unstyled
+                alt="Product image"
+                className=" object-cover"
+              />
+            </Box>
+          )}
+        </Flex>
+        <Flex justify="space-between" mb="sm">
+          <Box>
+            {index !== 0 && (
+              <IconChevronLeft
+                stroke={2}
+                color="white"
+                size={30}
+                onClick={handlePrev}
+                className="cursor-pointer  rounded-full bg-op bg-opacity-40 bg-black "
+              />
+            )}
+          </Box>
+          {index < data?.items?.length - 1 && (
+            <IconChevronRight
+              stroke={2}
+              color="white"
+              size={30}
+              onClick={handleNext}
+              className="cursor-pointer  rounded-full bg-op bg-opacity-40 bg-black "
+            />
+          )}
         </Flex>
       </Box>
 
