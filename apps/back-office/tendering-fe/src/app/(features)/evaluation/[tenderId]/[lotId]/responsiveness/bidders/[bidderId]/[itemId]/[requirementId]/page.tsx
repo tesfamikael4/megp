5842d78b2f698cbb8e01f6c63936e8a16904e128
Dagem Tsehay <dagemtsehay4@gmail.com>
2024-06-13 13:51:49 +0000

    Fix tendering opening

---
+++ apps/back-office/tendering-fe/src/app/(features)/evaluation/[tenderId]/[lotId]/responsiveness/bidders/[bidderId]/[itemId]/[requirementId]/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { Box, Flex } from '@mantine/core';
+import { PreviewDocument } from '@/app/(features)/evaluation/_components/preview-submitted-document';
+import { ComplianceAssessment } from '@/app/(features)/evaluation/_components/compliance';
+
+export default function ChecklistDetail() {
+  return (
+    <Flex gap={10}>
+      <Box className=" bg-white w-2/3">
+        <PreviewDocument milestone="technicalResponsiveness" />
+      </Box>
+      <Box className=" bg-white w-1/3">
+        <ComplianceAssessment milestone="technicalResponsiveness" />
+      </Box>
+    </Flex>
+  );
+}
---
+++ apps/back-office/tendering-fe/src/app/(features)/evaluation/[tenderId]/[lotId]/responsiveness/bidders/[bidderId]/[itemId]/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useLazyGetResponsivenessRequirementsByLotIdQuery } from '@/store/api/tendering/technical-responsiveness.api';
+import { Box, Loader } from '@mantine/core';
+import { logger } from '@megp/core-fe';
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function BiderDetail() {
+  const router = useRouter();
+  const [getRequirements, { data: requirementData, isSuccess }] =
+    useLazyGetResponsivenessRequirementsByLotIdQuery();
+  const { tenderId, lotId, bidderId, itemId } = useParams();
+
+  useEffect(() => {
+    getRequirements({
+      lotId: lotId as string,
+      bidderId: bidderId as string,
+      itemId: itemId as string,
+    });
+  }, []);
+
+  useEffect(() => {
+    logger.log({ requirementData });
+    if (requirementData && requirementData?.length > 0) {
+      router.push(
+        `/evaluation/${tenderId}/${lotId}/responsiveness/bidders/${bidderId}/${itemId}/${requirementData[0].checklist?.[0]?.id}`,
+      );
+    }
+  }, [bidderId, requirementData, isSuccess, lotId, router, tenderId]);
+  return (
+    <Box className="flex justify-center items-center h-full">
+      <Loader />
+    </Box>
+  );
+}
