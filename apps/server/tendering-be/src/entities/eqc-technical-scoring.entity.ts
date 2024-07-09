2f59f5631adcc1a59fa7344f0c6337dea1cb6caf
NathyG2524 <nathygirma2524@gmail.com>
2024-07-09 08:31:59 +0300

    fix(tendering-be): fix eqc scoring

--- apps/server/tendering-be/src/entities/eqc-technical-scoring.entity.ts
+++ apps/server/tendering-be/src/entities/eqc-technical-scoring.entity.ts
@@ -34,7 +34,7 @@ export class EqcTechnicalScoring extends Audit {
   children: EqcTechnicalScoring[];
 
   @TreeParent()
-  parent: EqcTechnicalScoring[];
+  parent: EqcTechnicalScoring;
 
   @Column()
   requirement: string;
